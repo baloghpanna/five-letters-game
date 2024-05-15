4c8a54909da5003df721667accfbf91a1de151f1
baloghpanna <balogh.p.develop@gmail.com>
2024-05-15 19:40:25 +0200

    intial commit

---
+++ src/main/java/hu/fivelettersgame/FiveLettersGameApplication.java
@@ -0,0 +1,13 @@
+package hu.fivelettersgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiveLettersGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FiveLettersGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/fivelettersgame/FiveLettersGameApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.fivelettersgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiveLettersGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
