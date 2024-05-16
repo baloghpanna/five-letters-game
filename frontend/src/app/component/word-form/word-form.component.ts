709fb2668f2c7e64b4b5fe1b2d97d7df693d7551
baloghpanna <balogh.p.develop@gmail.com>
2024-05-16 10:29:00 +0200

    add word-form component

---
+++ frontend/src/app/component/word-form/word-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-word-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './word-form.component.html',
+  styleUrl: './word-form.component.scss'
+})
+export class WordFormComponent {
+
+}
