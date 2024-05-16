e2cc288654b4e9ee9f88d221c0ccedb1e8a0f59c
baloghpanna <balogh.p.develop@gmail.com>
2024-05-16 12:16:02 +0200

    letters-box format2 - how can I make well responsive?

--- frontend/src/app/component/abc/abc.component.ts
+++ frontend/src/app/component/abc/abc.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef} from '@angular/core';
 import {MatCard} from "@angular/material/card";
 
 @Component({
@@ -10,6 +10,25 @@ import {MatCard} from "@angular/material/card";
   templateUrl: './abc.component.html',
   styleUrl: './abc.component.scss'
 })
-export class AbcComponent {
+export class AbcComponent implements AfterViewInit {
     abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F','G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z']
+    minBoxesPerRow: number = 0;
+
+  constructor(private elRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.calculateMinBoxesPerRow();
+    // Esetlegesen figyelj változásokra, amelyek újraszámolást igényelhetnek
+    // pl.: ablakméret változása
+    window.addEventListener('resize', () => {
+      this.calculateMinBoxesPerRow();
+    });
+  }
+
+  calculateMinBoxesPerRow() {
+    const containerWidth = this.elRef.nativeElement.querySelector('.abc-container').offsetWidth;
+    const boxWidth = this.elRef.nativeElement.querySelector('.letters-box').offsetWidth;
+    // Számold ki, hány betű-négyzet fér el egy sorban
+    this.minBoxesPerRow = Math.floor(containerWidth / boxWidth);
+  }
 }
