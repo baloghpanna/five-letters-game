40228528823fbdb7921d016991e5ec811e2e84b3
baloghpanna <balogh.p.develop@gmail.com>
2024-05-25 17:25:51 +0200

    i want flipped card - don't okay

--- frontend/src/app/component/abc/abc.component.ts
+++ frontend/src/app/component/abc/abc.component.ts
@@ -1,20 +1,25 @@
 import {AfterViewInit, Component, ElementRef} from '@angular/core';
 import {MatCard} from "@angular/material/card";
+import {NgClass} from "@angular/common";
 
 @Component({
   selector: 'app-abc',
   standalone: true,
   imports: [
-    MatCard
+    MatCard, NgClass
   ],
   templateUrl: './abc.component.html',
   styleUrl: './abc.component.scss'
 })
 export class AbcComponent implements AfterViewInit {
-    abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F','G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z']
-    minBoxesPerRow: number = 0;
+  abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z']
+  minBoxesPerRow: number = 0;
+  isFlipped = false;
 
-  constructor(private elRef: ElementRef) {}
+
+
+  constructor(private elRef: ElementRef) {
+  }
 
   ngAfterViewInit() {
     this.calculateMinBoxesPerRow();
@@ -30,5 +35,10 @@ export class AbcComponent implements AfterViewInit {
     const boxWidth = this.elRef.nativeElement.querySelector('.letters-box').offsetWidth;
     // Számold ki, hány betű-négyzet fér el egy sorban
     this.minBoxesPerRow = Math.floor(containerWidth / boxWidth);
+  }
+
+  toggleFlip() {
+      this.isFlipped = !this.isFlipped;
+
   }
 }
