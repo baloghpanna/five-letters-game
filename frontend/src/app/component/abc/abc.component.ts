76af5fb1b76781ee8603cd899ce604cae6a99ab0
baloghpanna <balogh.p.develop@gmail.com>
2024-05-26 14:14:24 +0200

    flipped card on click - with help of emer

--- frontend/src/app/component/abc/abc.component.ts
+++ frontend/src/app/component/abc/abc.component.ts
@@ -1,7 +1,11 @@
-import {AfterViewInit, Component, ElementRef} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
 import {MatCard} from "@angular/material/card";
 import {NgClass} from "@angular/common";
 
+interface Item {
+  name: string;
+  isFlipped: boolean;
+}
 @Component({
   selector: 'app-abc',
   standalone: true,
@@ -11,8 +15,9 @@ import {NgClass} from "@angular/common";
   templateUrl: './abc.component.html',
   styleUrl: './abc.component.scss'
 })
-export class AbcComponent implements AfterViewInit {
-  abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z']
+export class AbcComponent implements AfterViewInit, OnInit {
+  abc: Item[] = [];
+  // abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z']
   minBoxesPerRow: number = 0;
   // isFlipped = false;
   isFlipped: { [key: string]: boolean } = {};
@@ -20,6 +25,9 @@ export class AbcComponent implements AfterViewInit {
 
 
   constructor(private elRef: ElementRef) {
+  }
+  ngOnInit() {
+    this.abc = this.createItemsArray();
   }
 
   ngAfterViewInit() {
@@ -44,6 +52,19 @@ export class AbcComponent implements AfterViewInit {
   // }
   toggleFlip(item: string) {
     this.isFlipped[item] = !this.isFlipped[item];
+  }
+
+  setThis(item: Item, target: boolean) {
+    item.isFlipped = target;
+  }
+
+  createItemsArray(): Item[] {
+    const alphabet = 'AÁBCDEÉFGHIÍJKLMNOÓÖŐPRSTUÚÜŰVZ';
+    const itemsArray: Item[] = [];
+    for (const char of alphabet) {
+      itemsArray.push({ name: char, isFlipped: false });
+    }
+    return itemsArray;
   }
 
 }
