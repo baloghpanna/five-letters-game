a640ff6ac5eedd46f6ba07b031fa969d782bba96
baloghpanna <balogh.p.develop@gmail.com>
2024-07-17 15:26:10 +0200

    add gameId field + modified makeGuessTips parameters (off GameInput)

--- frontend/src/app/service/game.service.ts
+++ frontend/src/app/service/game.service.ts
@@ -2,7 +2,6 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {WordSecretModel} from "../model/wordSecret.model";
 import {BehaviorSubject, Observable} from "rxjs";
-import {WordListModel} from "../model/wordList.model";
 import {WordInputModel} from "../model/wordInput.model";
 import {GuessResultModel} from "../model/guessResult.model";
 import {GameInput} from "../model/gameInput.model";
@@ -13,30 +12,36 @@ const BASE_URL = 'http://localhost:8081/api/words'
   providedIn: 'root'
 })
 export class GameService {
+  private gameId: number = 0;
 
   constructor(private http: HttpClient) {
+  }
+
+  setGameId(id: number): void {
+    this.gameId = id;
+  }
+
+  getGameId(): number {
+    return this.gameId;
   }
 
   getSecretWord(): Observable<WordSecretModel> {
     return this.http.get<WordSecretModel>(BASE_URL);
   }
 
-  makeGuessTips(inputWord: WordInputModel, secretWord: WordSecretModel) {
-    // const url = `${BASE_URL}`;
-    const gameInput: GameInput = {
-      wordInput: inputWord,
-      wordSecret: secretWord
-    };
-    console.log("A beküldött szó: " + gameInput.wordInput.word + " A titkos szó: " + gameInput.wordSecret.secretWord);
-    // return this.http.post(url, gameInput);
-    return this.http.post(BASE_URL, gameInput);
+  makeGuessTips(inputWord: WordInputModel) {
+    console.log("A beküldött szó: " + inputWord.word + " ---gameID: " + this.gameId);
+    return this.http.post(`${BASE_URL}/${this.gameId}/guess`, inputWord);
   }
-
 
   private wordSecretObject = new BehaviorSubject<WordSecretModel>({secretWord: '', wordId: 0, gameId: 0});
   wordSecretModel$ = this.wordSecretObject.asObservable();
 
   updateSecretWord(data: WordSecretModel) {
     this.wordSecretObject.next(data);
+  }
+
+  getGuessList() : Observable<GuessResultModel[]> {
+    return this.http.get<GuessResultModel[]>(BASE_URL);
   }
 }
