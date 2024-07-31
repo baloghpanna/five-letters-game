4cdd87416e2be96b32775960352be0a12ba736d0
baloghpanna <balogh.p.develop@gmail.com>
2024-07-31 14:28:44 +0200

    begin drag and drop function - use CdkDropList - add one more array

--- frontend/src/app/component/abc/abc.component.ts
+++ frontend/src/app/component/abc/abc.component.ts
@@ -1,6 +1,14 @@
-import {AfterViewInit, Component, ElementRef, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {MatCard} from "@angular/material/card";
 import {NgClass} from "@angular/common";
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDragEnd,
+  CdkDragStart, CdkDropList,
+  moveItemInArray,
+  transferArrayItem
+} from "@angular/cdk/drag-drop";
 
 interface Item {
   name: string;
@@ -10,18 +18,17 @@ interface Item {
   selector: 'app-abc',
   standalone: true,
   imports: [
-    MatCard, NgClass
+    MatCard, NgClass, CdkDrag, CdkDropList
   ],
   templateUrl: './abc.component.html',
-  styleUrl: './abc.component.scss'
+  styleUrls: ['./abc.component.scss']
 })
 export class AbcComponent implements OnInit {
   abc: Item[] = [];
+  letters: Item[] = [];
   // abc = ['A', 'Á', 'B', 'C', 'D', 'E', 'É', 'F', 'G', 'H', 'I', 'Í', 'J', 'K', 'L', 'M', 'N', 'O', 'Ó', 'Ö', 'Ő', 'P', 'R', 'S', 'T', 'U', 'Ú', 'Ü', 'Ű', 'V', 'Z']
 
 
-  constructor() {
-  }
   ngOnInit() {
     this.abc = this.createItemsArray();
   }
@@ -39,4 +46,24 @@ export class AbcComponent implements OnInit {
     return itemsArray;
   }
 
+  drop(event: CdkDragDrop<Item[]>) {
+    if (event.previousContainer === event.container) {
+      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+    } else {
+      transferArrayItem(
+        event.previousContainer.data,
+        event.container.data,
+        event.previousIndex,
+        event.currentIndex,
+      );
+    }
+  }
+
+  onDragStart(event: CdkDragStart) {
+    event.source.element.nativeElement.classList.add('dragging');
+  }
+
+  onDragEnd(event: CdkDragEnd) {
+    event.source.element.nativeElement.classList.remove('dragging');
+  }
 }
