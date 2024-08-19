cb975ea116ce4b0aa9de819c2e8a5011835cf48d
baloghpanna <balogh.p.develop@gmail.com>
2024-08-19 08:40:25 +0200

    ControllerTest: testGetSecretWord()

--- src/test/java/hu/fivelettersgame/ControllerTest.java
+++ src/test/java/hu/fivelettersgame/ControllerTest.java
@@ -2,21 +2,25 @@ package hu.fivelettersgame;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import hu.fivelettersgame.controller.GameController;
+import hu.fivelettersgame.domain.dto.outgoing.WordSecret;
 import hu.fivelettersgame.exception.GlobalExceptionHandler;
 import hu.fivelettersgame.service.GameService;
 import hu.fivelettersgame.validator.WordInputValidator;
-import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.context.MessageSource;
 import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.mockito.Mockito.validateMockitoUsage;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ControllerTest {
@@ -24,13 +28,13 @@ public class ControllerTest {
     private MockMvc mockMvc;
 
     @Mock
-    private GameService gameService;
+    private GameService gameServiceMock;
 
     @BeforeEach
     public void setUp() {
         WordInputValidator wordInputValidator = new WordInputValidator();
 
-        GameController gameController = new GameController(gameService, wordInputValidator);
+        GameController gameController = new GameController(gameServiceMock, wordInputValidator);
 
         mockMvc = MockMvcBuilders.standaloneSetup(gameController)
                 .setControllerAdvice(new GlobalExceptionHandler(messageSource()))
@@ -41,6 +45,29 @@ public class ControllerTest {
     @AfterEach
     void validate() { validateMockitoUsage();}
 
+    
+    @Test
+    void testGetSecretWord() throws Exception {
+        WordSecret wordSecret = new WordSecret();
+        wordSecret.setSecretWord("halom");
+        wordSecret.setWordId(1L);
+        wordSecret.setGameId(1L);
+
+        when(gameServiceMock.getSecretWord()).thenReturn(wordSecret);
+
+        this.mockMvc.perform(get("/api/words"))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.secretWord").value("halom"))
+                .andExpect(jsonPath("$.wordId").value(1))
+                .andExpect(jsonPath("$.gameId").value(1));
+
+//                .andExpect(content().json(asJsonString(wordSecret)));
+
+        verify(gameServiceMock, times(1)).getSecretWord();
+
+        verifyNoMoreInteractions(gameServiceMock);
+    }
     private MessageSource messageSource() {
         ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
 
